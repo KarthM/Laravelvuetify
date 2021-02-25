4675efb64e0eaaf4057660e6b44da118035ffcb3
karthiga <karthiga.chitra@gmail.com>
2021-02-25 10:03:52 +0000

    first commit

---
+++ laravelVue -CRUD/resources/js/pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Dashboard</h2>
+        <p>It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.</p>
+         <p>It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.</p>
+          <p>It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dashboard',
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ laravelVue -CRUD/resources/js/pages/Settings.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Settings</h2>
+        <p>It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.It was discovered by researchers at security company Red Canary, who have yet to determine its purpose.</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dashboard',
+  };
+</script>
+
+<style scoped>
+
+</style>
